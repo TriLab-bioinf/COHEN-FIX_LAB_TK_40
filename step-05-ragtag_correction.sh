a4fb7200631b65f12fb159138062dfad6795c7ce
Hernan Lorenzi <Hernan.Lorenzi@nih.gov>
2023-01-24 14:09:57 -0500

    adding step-05-ragtag_correction.sh

---
+++ step-05-ragtag_correction.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+GENOME=$1
+GFF=$2
+CONTIGS=$3
+LIST=$4
+
+module load ragtag
+
+PARAMETERS="--remove-small --gff ${GFF} -o 05-ragtag_correction -t 16 -T sr -F ${LIST}"
+
+ragtag.py correct ${PARAMETERS} ${GENOME} ${CONTIGS}
